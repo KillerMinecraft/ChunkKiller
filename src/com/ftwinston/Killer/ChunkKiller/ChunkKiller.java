b1284df13b23d1c92340d9c6454fdb737fb7fc81
FTWinston <ftwinston@gmail.com>
2013-01-07 00:55:26 +0000

    (barebones) chunks are now generated, and their players placed upon them

--- src/com/ftwinston/Killer/ChunkKiller/ChunkKiller.java
+++ src/com/ftwinston/Killer/ChunkKiller/ChunkKiller.java
@@ -2,15 +2,19 @@ package com.ftwinston.Killer.ChunkKiller;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import com.ftwinston.Killer.GameMode;
-import com.ftwinston.Killer.Helper;
 import com.ftwinston.Killer.Option;
 
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.bukkit.World.Environment;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.generator.ChunkGenerator;
 
 public class ChunkKiller extends GameMode
 {
@@ -56,36 +60,119 @@ public class ChunkKiller extends GameMode
 	public boolean useDiscreetDeathMessages() { return false; }
 	
 	@Override
+	public Environment[] getWorldsToGenerate() { return new Environment[] { Environment.NORMAL }; }
+	
+	@Override
+	public ChunkGenerator getCustomChunkGenerator(int worldNumber)
+	{
+		// really should have a new method to be called here, beforeWorldGeneration(int worldNumber)
+			// get a list of all players, in a random order
+			List<Player> players = getOnlinePlayers();
+			Collections.shuffle(players);
+			
+			// set up an array of player names, so we can easily determine the index (and thus the chunk) of any given player
+			playerIndices = new String[players.size()];
+			for ( int i=0; i<playerIndices.length; i++ )
+				playerIndices[i] = players.get(i).getName();
+			
+			chunkRows = (int)Math.ceil(Math.sqrt(playerIndices.length));
+			chunkCols = (int)Math.ceil((double)playerIndices.length/chunkRows);
+			chunksOnLastRow = playerIndices.length % chunkRows;
+			if ( chunksOnLastRow == 0 )
+				chunksOnLastRow = chunkCols;
+			
+		
+		return new PlayerChunkGenerator();
+	}
+	
+	public class PlayerChunkGenerator extends ChunkGenerator
+	{
+		int chunkMinX, chunkMaxX, chunkMinZ, chunkMaxZ, lastRowChunk;
+		public PlayerChunkGenerator()
+		{
+			chunkMinX = 0; chunkMaxX = (chunkRows-1) * chunkSpacing;
+			chunkMinZ = 0; chunkMaxZ = (chunkCols-1) * chunkSpacing;
+			lastRowChunk = (chunksOnLastRow-1) * chunkSpacing;
+		}
+		
+		@Override
+		public byte[][] generateBlockSections(World world, Random random, int x, int z, BiomeGrid biomes)
+		{
+			if ( x < chunkMinX || x > chunkMaxX || z < chunkMinZ || z > chunkMaxZ || x % chunkSpacing != 0 || z % chunkSpacing != 0 || (x == chunkMaxX && z > lastRowChunk) )
+				return new byte[1][];
+			
+			// a player chunk goes here
+			final int numChunkSections = 4;
+			byte[][] data = new byte[numChunkSections][];
+			
+			// stone
+			for ( int i=0; i<numChunkSections; i++ )
+			{
+				data[i] = new byte[4096];
+				for ( int j=0; j<4096; j++ )
+					data[i][j] = 1;
+			}
+
+			// bedrock
+			int i = 0;
+			for ( int j=0; j<256; j++ )
+				data[i][j] = 7;
+			
+			// dirt
+			i = numChunkSections - 1;
+			for ( int j=2048; j<3840; j++ )
+				data[i][j] = 3;
+			
+			// grass
+			for ( int j=3840; j<4096; j++ )
+				data[i][j] = 2;
+			
+			return data;
+		}
+		
+		void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
+	        if (result[y >> 4] == null) {
+	            result[y >> 4] = new byte[4096];
+	        }
+	        result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;
+	    }
+	}
+	
+	@Override
 	public Location getSpawnLocation(Player player)
 	{
 		// each player should spawn on their own chunk ... unless they've been defeated, in which case they should spawn on their master's chunk
-		return Helper.getSafeSpawnLocationNear(getWorld(0).getSpawnLocation());
+
+		Chunk c = getChunkForPlayer(player);
+		
+		int x = (c.getX() << 4) + 7, z = (c.getZ() << 4) + 7;
+		System.out.println("x = " + x + ", z = " + z);
+		return c.getWorld().getHighestBlockAt(x, z).getRelative(BlockFace.UP).getLocation();
 	}
 	
 	String[] playerIndices;
-	int numPlayerChunkRows;
+	int chunkRows, chunkCols, chunksOnLastRow, chunkSpacing = 3;
 	
 	@Override
 	public void gameStarted(boolean isNewWorlds)
 	{
 		if ( !isNewWorlds )
 			; // this ... wouldn't work. Game mode ought to have a function for whether this is allowed at all??
-		
-		// get a list of all players, in a random order
-		List<Player> players = getOnlinePlayers();
-		Collections.shuffle(players);
-		
-		// set up an array of player names, so we can easily determine the index (and thus the chunk) of any given player
-		playerIndices = new String[players.size()];
-		for ( int i=0; i<playerIndices.length; i++ )
-			playerIndices[i] = players.get(i).getName();
-		
-		numPlayerChunkRows = (int)Math.ceil(Math.sqrt(playerIndices.length));
+	}
+	
+	public int getPlayerIndex(Player player)
+	{
+		for ( int i=0; i< playerIndices.length; i++ )
+			if ( player.getName().equals(playerIndices[i]) )
+				return i;
+		return 0;
 	}
 	
 	public Chunk getChunkForPlayer(Player player)
 	{
-		// use their name, get the index, then determine the chunk by means of numPlayerChunkRows 
+		int index = getPlayerIndex(player);
+		int row = index / chunkRows, col = index % chunkRows;
+		return getWorld(0).getChunkAt(row * chunkSpacing, col * chunkSpacing);
 	}
 	
 	@Override
